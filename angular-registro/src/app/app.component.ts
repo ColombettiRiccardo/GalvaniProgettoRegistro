eec4cd901d473dd70687cea6fbe02f09aaea7050
ColombettiRiccardo <colombetti.riccardo@iisgalvanimi.edu.it>
2023-09-29 08:51:53 +0000

    fatto

--- angular-registro/src/app/app.component.ts
+++ angular-registro/src/app/app.component.ts
@@ -1,23 +1,27 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Students } from './models/students.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  constructor(private http: HttpClient) { }
+  rizz: Students = {studenti:[]};
+
+  constructor(private http: HttpClient) {
+    this.getData();
+  }
 
   public getData(): void {
-    const url = 'http://example.com/api/data'; // Sostituisci con l'URL effettivo del server e dell'endpoint
+    const url =
+      'https://5000-studerriccardo-server-k0a5iqm3gnb.ws-eu105.gitpod.io/';
 
-    this.http.get(url).subscribe(
-      (response) => {
-        let ris =  response
-      },
-      (error) => {
-        console.error('Error:', error);
-      }
-    );
+    this.http.get<Students>(url).subscribe((response) => {
+      let ris = response;
+      console.log(ris);
+      this.rizz = ris
+    });
+    
   }
 }
